b100b36cadaa58eb93a6ed6167c14bc7c8c64f15
@ <oncwnuNmkZeBqeE5y9KOoWHOpWg0@git.weixin.qq.com>
2020-01-11 21:17:09 +0800

    pages
    add pages

---
+++ pages/1001/1001.js
@@ -0,0 +1,71 @@
+// pages/1001/1001.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    TabCur: "tab01",
+
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  NavTab: function (t) {
+    let _this = this;
+    _this.setData({
+      TabCur: t.currentTarget.dataset.cur,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/1003/1003.js
@@ -0,0 +1,40 @@
+// pages/1003/1003.js
+Page({
+  name: "index",
+  data: {
+    value: [2, 2],
+    btnTxt: "金牛女配金牛男",
+    conList: ["白羊", "金牛", "双子", "巨蟹", "狮子", "处女", "天秤", "天蝎", "射手", "摩羯", "水瓶", "双鱼"],
+    women: ["白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座"],
+    men: ["白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座"],
+    woman: 2,
+    man: 2
+  },
+  onLoad: function () { },
+  onReady: function () { },
+  onShow: function () { },
+  onHide: function () { },
+  onUnload: function () { },
+  onPullDownRefresh: function () { },
+  onShareAppMessage: function (n) { },
+
+  bindChange: function (n) {
+    var a = this;
+    this.setData({
+      woman: n.detail.value[0],
+      man: n.detail.value[1],
+      btnTxt: "" + a.data.conList[n.detail.value[0]] + "女配" + a.data.conList[n.detail.value[1]] + "男"
+    });
+  },
+
+  tap_start: function (n) {
+    //this.showModalVideo();
+    console.log('a: ' + this.data.woman + 'b: ' + this.data.man)
+    wx.navigateTo({
+      url: "10031?X=" + this.data.woman + "&Y=" + this.data.man
+    });
+
+  },
+
+
+});
