c33c2e2e9446909061c4adffd497728051ae85bf
@ <oncwnuNmkZeBqeE5y9KOoWHOpWg0@git.weixin.qq.com>
2020-01-12 12:03:13 +0800

    tt
    ttt

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,22 +15,8 @@ Page({
         id: '1003',
         sub: '1003',
         name: 'custom navigationBar'
-      },
-      {
-        id: '1004',
-        sub: '1004',
-        name: 'test'
-      },
-      {
-        id: '1005',
-        sub: '1005',
-        name: 'test'
-      },
-      {
-        id: '1006',
-        sub: '1006',
-        name: 'test'
       }
+      
     ]
   }
 })
